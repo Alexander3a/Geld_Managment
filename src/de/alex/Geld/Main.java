b954cd38b87c6e54d970143a93acccc33209cc4d
Alexander3a <Alexander3a00009@gmail.com>
2019-05-18 15:27:49 +0200

    Added Config intpelementend auto updater handler Added Msql template

--- src/de/alex/Geld/Main.java
+++ src/de/alex/Geld/Main.java
@@ -1,14 +1,27 @@
 package de.alex.Geld;
 
+import java.nio.charset.Charset;
+import java.util.Random;
+
 public class Main {
     public static String currend_db = "";
     public static LoginUI loginUI;
 
     public static void main(String[] args) {
 	// write your code here
-        String[] db_list = new String[2];
-        db_list[0]= "gay";
-        db_list[1]= "test";
-        loginUI = new LoginUI(db_list);
+        if(Msql.isConnected()) {
+            updater.ini();
+            updater.Start();
+
+
+            if (!updater.isupdating()) {
+                String[] db_list = new String[2];
+                db_list[0] = "gay";
+                db_list[1] = "test";
+                loginUI = new LoginUI(db_list);
+            }else {
+                System.out.println("updating now");
+            }
+        }
     }
 }
---
+++ src/de/alex/Geld/updater.java
@@ -0,0 +1,37 @@
+package de.alex.Geld;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class updater {
+    private static Boolean updating = false;
+    private static Thread updater_thread = new Thread();
+    public static void Start(){
+        updater_thread.run();
+    }
+    public static void Stop(){
+        updater_thread.interrupt();
+    }
+    public static void ini(){
+        updater_thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    PreparedStatement ps = Msql.con.prepareStatement("SE");
+                    ps.setString(1,"asd");
+                    ResultSet rs = ps.executeQuery();
+                    while(rs.next()){
+
+                    }
+                }catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    public static Boolean isupdating(){
+        return updating;
+    }
+
+}
